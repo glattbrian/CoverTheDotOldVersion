1ea8c41053d3b23c50bbe573eb0e56e542035c56
Brian Glatt <brianglatt@192.168.1.13>
2016-05-12 23:08:44 -0400

    Initial Commit

---
+++ TempGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TempGame
+//
+//  Created by Brian Glatt on 5/12/16.
+//  Copyright © 2016 Brian Glatt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TempGame/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  TempGame
+//
+
+//  Copyright (c) 2016 Brian Glatt. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
